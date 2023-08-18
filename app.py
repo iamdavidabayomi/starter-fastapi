d3b7c9977ea5e29676efca7b8811a669f954e7dd
David Abayomi E <iamdavidabayomi@gmail.com>
2023-08-18 02:36:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,40 +3,14 @@ from fastapi.responses import FileResponse
 import google.auth.transport.requests
 from google.oauth2 import service_account
 
-from pydantic import BaseModel
-
 app = FastAPI()
 
 SCOPES = ['https://www.googleapis.com/auth/firebase.messaging']
 
 
-class Item(BaseModel):
-    item_id: int
-
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-
-@app.get('/favicon.ico', include_in_schema=False)
-async def favicon():
-    return FileResponse('favicon.ico')
-
-
-@app.get("/item/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-@app.get("/items/")
-async def list_items():
-    return [{"item_id": 1, "name": "Foo"}, {"item_id": 2, "name": "Bar"}]
-
-
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
+    return {"details": "no details"}
 
 @app.get("/get-access-token")
 async def get_access_token():
